ce1da7f8437b09535502b2f39939b0788c2a97b4
jjcallah@ncsu.edu <45086295+JackCallahan@users.noreply.github.com>
2024-04-13 19:26:54 -0400

    Cursor for controller

---
+++ Assets/Scripts/VirtualMouseUI.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.InputSystem.UI;
+
+public class VirtualMouseUI : MonoBehaviour
+{
+    [SerializeField] private RectTransform canvasRectTransform;
+    private VirtualMouseInput virtualMouseInput;
+
+    private void Awake()
+    {
+        virtualMouseInput = GetComponent<VirtualMouseInput>();
+    }
+        
+    private void Update()
+    {
+        transform.localScale = Vector3.one * (1f / canvasRectTransform.localScale.x);
+        transform.SetAsLastSibling();
+    }
+
+    private void LateUpdate()
+    {
+        Vector2 virtualMousePosition = virtualMouseInput.virtualMouse.position.value;
+        virtualMousePosition.x = Mathf.Clamp( virtualMousePosition.x, 0f, Screen.width );
+        virtualMousePosition.y = Mathf.Clamp( virtualMousePosition.y, 0f, Screen.height );
+        InputState.Change( virtualMouseInput.virtualMouse.position, virtualMousePosition );
+    }
+}
+
